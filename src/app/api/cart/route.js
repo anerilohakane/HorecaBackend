af6962a7d336392d59132fb5930085648955f1ac
Chaitanya-delxn <chaitanyakhairmodedelxn@gmail.com>
2026-04-14 17:03:17 +0530

    Display Price of product

--- src/app/api/cart/route.js
+++ src/app/api/cart/route.js
@@ -56,6 +56,7 @@ export async function GET(req) {
             p?.image ||
             "/images/placeholder-product.png",
           stockQuantity: p?.stockQuantity,
+          categoryPrices: p?.categoryPrices,
         },
       };
     });
